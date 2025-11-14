b7cb0be52d62798f55fd9bb2c686655210585529
kamrawr <rawrdog92@yahoo.com>
2025-11-13 23:10:48 -0800

    Clean up repository: remove all legacy and backup files

--- js/results.js
+++
@@ -1,9 +0,0 @@
-// Results display module
-// Note: Core results logic is integrated into screening.js
-// This file is available for future enhancements
-
-// Future enhancements could include:
-// - PDF generation of results
-// - Email results to user
-// - Print-friendly formatting
-// - Save progress functionality
